68076ef4e512bb83b48384e1e1ac1933a98d214b
Ben Lye <ben@lye.co.nz>
2023-08-21 22:13:18 +0100

    Example config updates

--- SunsynkIHD/src/Config_Example.h
+++ SunsynkIHD/src/Config_Example.h
@@ -18,8 +18,8 @@
  */
 #define SUNSYNK_USERNAME    "<Your username for the Sunsynk app>"   // Username for Sunsynk API
 #define SUNSYNK_PASSWORD    "<Your password for the Sunsynk app>"   // Password for Sunsynk API
-#define SUNSYNK_PLANT_ID    "<The ID of your Sunsynk plant>"        // Numeric ID of the Sunsynk plant
-#define API_READ_TIMEOUT    5000                                    // API read timeout in milliseconds (default is 5000)
+#define SUNSYNK_PLANT_ID    <The ID of your Sunsynk plant>          // Numeric ID of the Sunsynk plant
+#define API_READ_TIMEOUT    10000                                   // API read timeout in milliseconds (default is 5000)
 
 /*
  * Timezone and NTP server settings
@@ -43,47 +43,46 @@
 #define LCD_BRIGHTNESS_NIGHT    0                                   // Night setting (0-255). At 0 the API polling will be suspended while night mode is enabled.
 
 /*
+ * Settings for the DIY ESP32-S3 DevKit with ILI9488 3.5" touchscreen
+ * If you are have built your own DIY IHF then adapt to suit your own implementation.
+ * If you are using an Elecrow display you can ignore the remainder of this file.
+*/
+#ifdef ESP32_ILI9488_SPI_TFT
+
+// Touchpad calibration data
+#define TOUCH_X_MIN          100
+#define TOUCH_X_MAX         4000
+#define TOUCH_Y_MIN          100
+#define TOUCH_Y_MX          4000
+
+// LCD Rotation
+#define LCD_ROTATION        3
+
+// LCD pins
+#define SPI_HOST_DEV        SPI3_HOST
+#define LCD_DC              6   // DC/RS
+#define LCD_CS              10  // CS
+#define LCD_SCK             12  // SCK
+#define LCD_MOSI            11  // SDI (MOSI)
+#define LCD_MISO            13  // T_DO
+#define LCD_BL              7   // LED
+#define LCD_RST             5   // RST
+
+// Touch pins
+#define TOUCH_SCK           12  // T_CLK
+#define TOUCH_MISO          13  // T_DO
+#define TOUCH_MOSI          11  // T_DIN
+#define TOUCH_CS            15  // T_CS
+#define TOUCH_INT           -1  // T_IRQ
+
+#endif // ESP32_ILI9488_SPI_TFT
+
+/*
  * Colours for PV, grid, battery, and load Watts
  */
 #define UI_GREEN    0x00E05A    // Color used for generating, charging, exporting
 #define UI_RED      0xE00000    // Color used for consuming, discharging, importing
 #define UI_GREY     0xA2A2A2    // Color used for no activity
 #define UI_WHITE    0x000000    // White
-
-
-/*
- * Settings for the DIY ESP32S3 DevKit with ILI9488 3.5" touchscreen
- * If you are have built your own DIY IHF then adapt to suit your own implementation.
- * If you are using an Elecrow display you can ignore the remainder of this file.
-*/
-#ifdef ESP32_ILI9488_SPI_TFT
-
-/*
- * Touchpad calibration data
- */
-#define TOUCH_MAP_X1         100
-#define TOUCH_MAP_X2        4000
-#define TOUCH_MAP_Y1         100
-#define TOUCH_MAP_Y2        4000
-
-// LCD Rotation
-#define LCD_ROTATION        3
-
-// LCD pins
-#define LCD_DC              0
-#define LCD_CS              15
-#define LCD_SCK             18
-#define LCD_MOSI            23
-#define LCD_MISO            GFX_NOT_DEFINED
-#define LCD_BL              32
-#define LCD_RST             4
-
-// Touch pins
-#define TOUCH_SCK           18
-#define TOUCH_MISO          19
-#define TOUCH_MOSI          23
-#define TOUCH_CS            21
-#define TOUCH_INT           27
-#endif // ESP32_ILI9488_SPI_TFT
 
 #endif // __CONFIG_H
