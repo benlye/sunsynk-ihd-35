c90ad87c8b851c16dfce0a1aa06b37ea7e7cde8d
Ben Lye <ben@lye.co.nz>
2023-12-22 13:31:02 +0000

    3.5" pin names and spacing

--- SunsynkIHD/include/esp32s3_elecrow_hmi_35.h
+++ SunsynkIHD/include/esp32s3_elecrow_hmi_35.h
@@ -28,44 +28,44 @@ public:
       auto cfg = _bus_instance.config();
 
       cfg.freq_write = 40000000;
-      cfg.pin_wr = 18;
-      cfg.pin_rd = 48;
-      cfg.pin_rs = 45;
+      cfg.pin_wr  = GPIO_NUM_18;
+      cfg.pin_rd  = GPIO_NUM_48;
+      cfg.pin_rs  = GPIO_NUM_45;
 
-      cfg.pin_d0 = 47;
-      cfg.pin_d1 = 21;
-      cfg.pin_d2 = 14;
-      cfg.pin_d3 = 13;
-      cfg.pin_d4 = 12;
-      cfg.pin_d5 = 11;
-      cfg.pin_d6 = 10;
-      cfg.pin_d7 = 9;
-      cfg.pin_d8 = 3;
-      cfg.pin_d9 = 8;
-      cfg.pin_d10 = 16;
-      cfg.pin_d11 = 15;
-      cfg.pin_d12 = 7;
-      cfg.pin_d13 = 6;
-      cfg.pin_d14 = 5;
-      cfg.pin_d15 = 4;
+      cfg.pin_d0  = GPIO_NUM_47;
+      cfg.pin_d1  = GPIO_NUM_21;
+      cfg.pin_d2  = GPIO_NUM_14;
+      cfg.pin_d3  = GPIO_NUM_13;
+      cfg.pin_d4  = GPIO_NUM_12;
+      cfg.pin_d5  = GPIO_NUM_11;
+      cfg.pin_d6  = GPIO_NUM_10;
+      cfg.pin_d7  = GPIO_NUM_9;
+      cfg.pin_d8  = GPIO_NUM_3;
+      cfg.pin_d9  = GPIO_NUM_8;
+      cfg.pin_d10 = GPIO_NUM_16;
+      cfg.pin_d11 = GPIO_NUM_15;
+      cfg.pin_d12 = GPIO_NUM_7;
+      cfg.pin_d13 = GPIO_NUM_6;
+      cfg.pin_d14 = GPIO_NUM_5;
+      cfg.pin_d15 = GPIO_NUM_4;
       _bus_instance.config(cfg);
       _panel_instance.bus(&_bus_instance);
     }
 
     {
       auto cfg = _panel_instance.config();
-      cfg.panel_width     =   320;
-      cfg.panel_height    =   480;
-      cfg.offset_x        =     0;
-      cfg.offset_y        =     0;
-      cfg.pin_cs          =    -1;
-      cfg.pin_rst         =    -1;
-      cfg.pin_busy        =    -1;
-      cfg.offset_rotation =     3;
-      cfg.readable        =  true;
+      cfg.panel_width     = 320;
+      cfg.panel_height    = 480;
+      cfg.offset_x        = 0;
+      cfg.offset_y        = 0;
+      cfg.pin_cs          = GPIO_NUM_NC;
+      cfg.pin_rst         = GPIO_NUM_NC;
+      cfg.pin_busy        = GPIO_NUM_NC;
+      cfg.offset_rotation = 3;
+      cfg.readable        = true;
       cfg.invert          = false;
       cfg.rgb_order       = false;
-      cfg.dlen_16bit      =  true;
+      cfg.dlen_16bit      = true;
       cfg.bus_shared      = false;
 
       _panel_instance.config(cfg);
@@ -84,9 +84,9 @@ public:
       auto cfg = _touch_instance.config();
       cfg.i2c_port = 0;
       cfg.i2c_addr = 0x38;
-      cfg.pin_sda  = 38;
-      cfg.pin_scl  = 39;
-      cfg.pin_int  = -1;
+      cfg.pin_sda  = GPIO_NUM_38;
+      cfg.pin_scl  = GPIO_NUM_39;
+      cfg.pin_int  = GPIO_NUM_NC;
       cfg.freq  = 400000;
       cfg.x_min = 0;
       cfg.x_max = 319;
